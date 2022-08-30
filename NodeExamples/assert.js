7f6efdcb820129dfc59aef7aefb4a98272da0b0f
unknown <sandesh.prabhu@robosoftin.com>
2022-08-30 17:46:55 +0530

    added more functions

--- NodeExamples/assert.js
+++ NodeExamples/assert.js
@@ -11,3 +11,30 @@ catch{
 
 assert(x>y)
 // error:The expression evaluated to a falsy value: assert(x>y)
+
+try{
+    assert(0)
+    console.log("No Error")
+}
+catch(error){
+    console.log("Error:"+error)
+}
+// Error:AssertionError [ERR_ASSERTION]: The expression evaluated to a falsy value: assert(0)
+
+try{
+    assert(1)
+    console.log("No Error")
+}
+catch(error){
+    console.log("Error:"+error)
+}
+// No Error
+
+// assert.deepStrictEqual(actual, expected[, message])
+try{
+    assert.deepStrictEqual({a:5},{a:'5'})
+    console.log("No Error")
+}
+catch(error){
+    console.log("Error:"+error)
+}
