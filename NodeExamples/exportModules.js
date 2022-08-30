56b27f57988e032f278f055212e4093aaee188a0
unknown <sandesh.prabhu@robosoftin.com>
2022-08-30 09:55:50 +0530

    initial commit

---
+++ NodeExamples/exportModules.js
@@ -0,0 +1,8 @@
+let data={
+    date: new Date().toLocaleDateString(),
+    sayHi:()=>{
+        console.log("HI!!")
+    },
+    sayBye:"Bye"
+}
+module.exports=data
---
+++ NodeExamples/importModule.js
@@ -0,0 +1,4 @@
+let inputModule=require('./exportModules.js')
+inputModule.sayHi()
+console.log(inputModule.date)
+console.log(inputModule.sayBye)
