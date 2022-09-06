b7eaadbbb151d57b558860bb2b81569075ba601f
unknown <sandesh.prabhu@robosoftin.com>
2022-09-06 17:54:44 +0530

    initial commit

---
+++ mongoose/firstFile.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose')
+main().catch(err => console.log(err))
+async function main(){
+    // await mongoose.connect('mongodb://localhost:27017/test')
+
+    let kittenSchema=new mongoose.Schema({
+        name:String
+    });
+    kittenSchema.methods.speak=function speak(){
+        let msg=this.name ? `My name is ${this.name}.` : "I don't have a name."
+        console.log(msg)
+    }
+    let Kitten=mongoose.model('Kitten',kittenSchema)
+    
+    let Vineeth=new Kitten({name:"Vineeth"})
+    console.log(Vineeth.name)
+    Vineeth.speak()
+    
+    let Fluffy=new Kitten({name:'Fluffy'})
+    Fluffy.speak()
+
+    let allKittens=await Kitten.find()
+    console.log(allKittens)
+}
